c0b9198bb48997a307cd3671122f6b2abe1efa5e
efraim urel <efraimurelpalodang@gmail.com>
2026-03-10 12:01:34 +0800

    selesai tapi masih banyak kurangnya wkwk

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Form from './components/Form'
+import Container from './components/Container'
+import NoteList from './components/NoteList'
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  const tambahCatatan = catatanBaru => {
+    setNotes([
+      ...notes,
+      catatanBaru
+    ]);
+  }
+
   return (
-    <div>App</div>
-  )
+    <>
+      <Container>
+        <Form tambahCatatan={tambahCatatan} />
+        <NoteList notes={notes}/>
+      </Container>
+    </>
+  );
 }
 
 export default App
